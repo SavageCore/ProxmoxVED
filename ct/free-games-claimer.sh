a8f645df4a3a70de436557123ba3f10bba528637
SavageCore <talk@savagecore.uk>
2026-02-05 20:21:47 +0000

    fix: Duplication in source url

--- ct/free-games-claimer.sh
+++ ct/free-games-claimer.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# Copyright (c) 2021-2025 community-scripts ORG
+# Copyright (c) 2021-2026 community-scripts ORG
 # Author: SavageCore
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://github.com/https://github.com/vogler/free-games-claimer
+# Source: https://github.com/vogler/free-games-claimer
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 
 APP="free-games-claimer"
